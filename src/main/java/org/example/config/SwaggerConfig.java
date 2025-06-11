b66c163ef51001eb846f2414e6e833a20f44f870
J <kj5592305@gmail.com>
2025-06-11 12:41:03 +0900

    feat: SwaggerConfig 파일 수정

--- src/main/java/org/example/config/SwaggerConfig.java
+++ src/main/java/org/example/config/SwaggerConfig.java
@@ -1,15 +1,24 @@
 package org.example.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "JWT",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("JWT API").version("1.0"))
+                .components(new Components().addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
 }
